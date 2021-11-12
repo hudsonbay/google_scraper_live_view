03739229cfd89a00b1bb843d3537396ff5513761
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-12 15:24:14 -0500

    feat: :art: Adde uploads folder to static files

--- lib/google_scraper_web/endpoint.ex
+++ lib/google_scraper_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule GoogleScraperWeb.Endpoint do
     at: "/",
     from: :google_scraper,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt uploads)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
