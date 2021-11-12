6121a9ca928ff62921bb405e10108cdcd3ccf0f1
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-12 15:57:49 -0500

    test: :fire: Corrected existing page_controller test

--- test/google_scraper_web/controllers/page_controller_test.exs
+++ test/google_scraper_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GoogleScraperWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Google web scraper"
   end
 end
