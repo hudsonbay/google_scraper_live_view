1d6052a90f4d686aa039d4204cd47e771824fda3
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-12 14:31:29 -0500

    feat: :sparkles: Installed dependencies for dealing qith CSV files

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule GoogleScraper.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:csv, "~> 2.4"}
     ]
   end
 
