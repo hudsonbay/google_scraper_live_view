ec7c20fb96623da7bfaa9f289cebd2c02f6be80c
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-14 18:06:20 -0500

    feat: :sparkles: Created function to bulk create keywords in the database

--- lib/google_scraper/keywords.ex
+++ lib/google_scraper/keywords.ex
@@ -101,4 +101,38 @@ defmodule GoogleScraper.Keywords do
   def change_keyword(%Keyword{} = keyword, attrs \\ %{}) do
     Keyword.changeset(keyword, attrs)
   end
+
+  def bulk_create_keywords(entity, attrs) do
+    attrs
+    |> Enum.map(fn entry ->
+      entity.changeset(entity.__struct__(), entry)
+    end)
+    |> Enum.with_index()
+    |> Enum.reduce(
+      Ecto.Multi.new(),
+      fn {changeset, index}, multi ->
+        Ecto.Multi.insert(multi, Integer.to_string(index), changeset, on_conflict: :nothing)
+      end
+    )
+    |> Repo.transaction()
+    |> case do
+      {:ok, _} ->
+        %{rows_affected: length(attrs), errors: []}
+
+      {
+        :error,
+        _,
+        %Ecto.Changeset{
+          changes: changes,
+          errors: errors
+        },
+        _
+      } ->
+        %{
+          rows_affected: 0,
+          errors: "Changes #{changes} couldn't be created because #{inspect(errors)}"
+        }
+    end
+  end
+
 end
