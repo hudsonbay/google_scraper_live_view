a99ef4bb19f40f780b3e146944c266f7d395cba7
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-14 18:25:49 -0500

    refactor: :fire: Deleted unused functions and tests

--- lib/google_scraper/keywords.ex
+++ lib/google_scraper/keywords.ex
@@ -21,87 +21,6 @@ defmodule GoogleScraper.Keywords do
     Repo.all(Keyword)
   end
 
-  @doc """
-  Gets a single keyword.
-
-  Raises `Ecto.NoResultsError` if the Keyword does not exist.
-
-  ## Examples
-
-      iex> get_keyword!(123)
-      %Keyword{}
-
-      iex> get_keyword!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_keyword!(id), do: Repo.get!(Keyword, id)
-
-  @doc """
-  Creates a keyword.
-
-  ## Examples
-
-      iex> create_keyword(%{field: value})
-      {:ok, %Keyword{}}
-
-      iex> create_keyword(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_keyword(attrs \\ %{}) do
-    %Keyword{}
-    |> Keyword.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a keyword.
-
-  ## Examples
-
-      iex> update_keyword(keyword, %{field: new_value})
-      {:ok, %Keyword{}}
-
-      iex> update_keyword(keyword, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_keyword(%Keyword{} = keyword, attrs) do
-    keyword
-    |> Keyword.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a keyword.
-
-  ## Examples
-
-      iex> delete_keyword(keyword)
-      {:ok, %Keyword{}}
-
-      iex> delete_keyword(keyword)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_keyword(%Keyword{} = keyword) do
-    Repo.delete(keyword)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking keyword changes.
-
-  ## Examples
-
-      iex> change_keyword(keyword)
-      %Ecto.Changeset{data: %Keyword{}}
-
-  """
-  def change_keyword(%Keyword{} = keyword, attrs \\ %{}) do
-    Keyword.changeset(keyword, attrs)
-  end
-
   def bulk_create_keywords(entity, attrs) do
     attrs
     |> Enum.map(fn entry ->
@@ -134,5 +53,4 @@ defmodule GoogleScraper.Keywords do
         }
     end
   end
-
 end
--- test/google_scraper/keywords_test.exs
+++ test/google_scraper/keywords_test.exs
@@ -8,79 +8,9 @@ defmodule GoogleScraper.KeywordsTest do
 
     import GoogleScraper.KeywordsFixtures
 
-    @invalid_attrs %{
-      html_content: nil,
-      name: nil,
-      total_advertisers: nil,
-      total_links: nil,
-      total_results: nil
-    }
-
     test "list_keywords/0 returns all keywords" do
       keyword = keyword_fixture()
       assert Keywords.list_keywords() == [keyword]
-    end
-
-    test "get_keyword!/1 returns the keyword with given id" do
-      keyword = keyword_fixture()
-      assert Keywords.get_keyword!(keyword.id) == keyword
-    end
-
-    test "create_keyword/1 with valid data creates a keyword" do
-      valid_attrs = %{
-        html_content: "some html_content",
-        name: "some name",
-        total_advertisers: 42,
-        total_links: 42,
-        total_results: "Cerca de 923,000 resultados (0.54 segundos)"
-      }
-
-      assert {:ok, %Keyword{} = keyword} = Keywords.create_keyword(valid_attrs)
-      assert keyword.html_content == "some html_content"
-      assert keyword.name == "some name"
-      assert keyword.total_advertisers == 42
-      assert keyword.total_links == 42
-      assert keyword.total_results == "Cerca de 923,000 resultados (0.54 segundos)"
-    end
-
-    test "create_keyword/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Keywords.create_keyword(@invalid_attrs)
-    end
-
-    test "update_keyword/2 with valid data updates the keyword" do
-      keyword = keyword_fixture()
-
-      update_attrs = %{
-        html_content: "some updated html_content",
-        name: "some updated name",
-        total_advertisers: 43,
-        total_links: 43,
-        total_results: "Cerca de 923,000 resultados (0.54 segundos)"
-      }
-
-      assert {:ok, %Keyword{} = keyword} = Keywords.update_keyword(keyword, update_attrs)
-      assert keyword.html_content == "some updated html_content"
-      assert keyword.name == "some updated name"
-      assert keyword.total_advertisers == 43
-      assert keyword.total_links == 43
-      assert keyword.total_results == "Cerca de 923,000 resultados (0.54 segundos)"
-    end
-
-    test "update_keyword/2 with invalid data returns error changeset" do
-      keyword = keyword_fixture()
-      assert {:error, %Ecto.Changeset{}} = Keywords.update_keyword(keyword, @invalid_attrs)
-      assert keyword == Keywords.get_keyword!(keyword.id)
-    end
-
-    test "delete_keyword/1 deletes the keyword" do
-      keyword = keyword_fixture()
-      assert {:ok, %Keyword{}} = Keywords.delete_keyword(keyword)
-      assert_raise Ecto.NoResultsError, fn -> Keywords.get_keyword!(keyword.id) end
-    end
-
-    test "change_keyword/1 returns a keyword changeset" do
-      keyword = keyword_fixture()
-      assert %Ecto.Changeset{} = Keywords.change_keyword(keyword)
     end
   end
 end
