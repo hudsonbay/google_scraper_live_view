c791f2d0c7d026e67d600a1e90fda595c9388205
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-14 23:52:17 -0500

    style: :art: Fixed typo

--- lib/google_scraper_web/components/search_component.ex
+++ lib/google_scraper_web/components/search_component.ex
@@ -7,7 +7,7 @@ defmodule SearchComponent do
       <form phx-change="search">
         <div class="pt-2 relative mx-auto text-gray-600">
             <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                type="search" name="keyword" placeholder="Search by keyboard name">
+                type="search" name="keyword" placeholder="Search by keyword name">
             <div class="absolute right-0 top-0 mt-5 mr-4">
                 <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
