62ed1c43b6eeeceae921cd98669f38479a6df6f3
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-17 03:38:47 -0500

    perf: :zap: Added crawly to the applications list

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule GoogleScraper.MixProject do
   def application do
     [
       mod: {GoogleScraper.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :crawly]
     ]
   end
 
