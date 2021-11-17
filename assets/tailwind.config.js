04828b7bbbf1b5e337e615eca7befd8b61f599e5
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-17 00:04:30 -0500

    style: :art: Enhanced design with Tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './js/**/*.js',
-    '../lib/*_web/**/*.*ex'
-  ],
-  theme: {},
+  mode: "jit",
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  theme: {
+    container: {
+      center: true,
+    },
+  },
   variants: {},
-  plugins: []
+  plugins: [],
 };
--- lib/google_scraper_web/components/data_component.ex
+++ lib/google_scraper_web/components/data_component.ex
@@ -5,28 +5,73 @@ defmodule DataComponent do
     ~H"""
     <div class="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
 
-      <%=live_component SearchComponent, id: :search %>
+        <%=live_component SearchComponent, id: :search %>
 
-          <table class="table-auto border border-collapse">
-            <thead>
-              <tr>
-                <th class="border-2 border-black-600">KEYWORD</th>
-                <th class="border-2">ADVERTISERS</th>
-                <th class="border-2">LINKS</th>
-                <th class="border-2">RESULTS</th>
-              </tr>
-            </thead>
-            <tbody>
-            <%= for keyword <- @keywords do %>
-            <tr class="bg-emerald-200">
-              <td class="border"><%= keyword.name %></td>
-              <td class="border"><%= keyword.total_advertisers %></td>
-              <td class="border"><%= keyword.total_links %></td>
-              <td class="border"><%= keyword.total_results %></td>
-            </tr>
-            <% end %>
-            </tbody>
-          </table>
+
+            <div class="flex flex-col">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Keyword
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Advertisers
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Links
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Results
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <%= for keyword <- @keywords do %>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <%= keyword.name %>
+                                                </div>
+
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                              <div class="ml-4">
+                                                  <div class="text-sm text-gray-900">
+                                                      <%= keyword.total_advertisers %>
+                                                  </div>
+                                                </div>
+                                            </div>
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                          <div class="ml-4">
+                                              <div class="text-sm text-gray-900">
+                                                  <%= keyword.total_links %>
+                                              </div>
+                                            </div>
+                                        </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            <%= keyword.total_results %>
+                                        </td>
+                                    </tr>
+                                    <% end %>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
     </div>
     """
   end
