d5a859ec2a93bdd9e952a1a4ba46f6460c536fb9
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-18 00:55:31 -0500

    test: :fire: Refactored test to have a cleaner code

--- test/google_scraper/keywords_test.exs
+++ test/google_scraper/keywords_test.exs
@@ -2,15 +2,12 @@ defmodule GoogleScraper.KeywordsTest do
   use GoogleScraper.DataCase
 
   alias GoogleScraper.Keywords
+  alias GoogleScraper.KeywordsFixtures
 
   describe "keywords" do
-    alias GoogleScraper.Keywords.Keyword
-
-    import GoogleScraper.KeywordsFixtures
-
     test "list_keywords/0 returns all keywords" do
-      keyword = keyword_fixture()
-      assert Keywords.list_keywords() == [keyword]
+      {user_id, keyword} = KeywordsFixtures.keyword_fixture()
+      assert Keywords.list_keywords_by_user(user_id) == [keyword]
     end
   end
 end
