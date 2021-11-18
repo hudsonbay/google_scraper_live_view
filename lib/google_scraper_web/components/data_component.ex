22ee211c8df4d802fe0879824e005b05ebb4aa95
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-18 01:08:00 -0500

    style: :art: Fixed default table width

--- lib/google_scraper_web/components/data_component.ex
+++ lib/google_scraper_web/components/data_component.ex
@@ -8,7 +8,7 @@ defmodule DataComponent do
         <%=live_component SearchComponent, id: :search %>
 
             <div class="flex flex-col">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="py-2 align-middle inline-block min-w-full">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
