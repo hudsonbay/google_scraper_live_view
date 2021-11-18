e62a1f36144c3b93726ef396060832b1f80d9f74
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-17 23:06:03 -0500

    style: :art: Improved UI design

--- lib/google_scraper_web/components/data_component.ex
+++ lib/google_scraper_web/components/data_component.ex
@@ -7,27 +7,22 @@ defmodule DataComponent do
 
         <%=live_component SearchComponent, id: :search %>
 
-
             <div class="flex flex-col">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
-                                    <th scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="table-header-column-title">
                                         Keyword
                                     </th>
-                                    <th scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="table-header-column-title">
                                         Advertisers
                                     </th>
-                                    <th scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="table-header-column-title">
                                         Links
                                     </th>
-                                    <th scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="table-header-column-title">
                                         Results
                                     </th>
                                 </tr>
