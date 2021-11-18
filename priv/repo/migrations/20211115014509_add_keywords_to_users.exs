72d147f0318ecd811d87dea06e8534c4326afb8f
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-17 23:25:52 -0500

    style: :art: Formatted code

--- priv/repo/migrations/20211115014509_add_keywords_to_users.exs
+++ priv/repo/migrations/20211115014509_add_keywords_to_users.exs
@@ -3,11 +3,9 @@ defmodule GoogleScraper.Repo.Migrations.AddKeywordsToUsers do
 
   def change do
     alter table(:keywords) do
-      add :user_id, references(:users, on_delete: :delete_all),
-        null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
     end
 
     create index(:keywords, [:user_id])
-
   end
 end
